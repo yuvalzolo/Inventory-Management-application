6c272b6afc35382df34fd3cf21e0982c8f2f1e8a
yuvalzolo <45453175+yuvalzolo@users.noreply.github.com>
2020-09-20 18:55:46 +0300

    Update app.js
    solve cors problem

--- backend/app.js
+++ backend/app.js
@@ -9,13 +9,16 @@ app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 //Handle cors
-app.use((req, res, next)=>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers','*');
-    if(req.method==='OPTIONS'){
-        req.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id");
+
+    res.header(
+        'Access-Control-Expose-Headers',
+        'x-access-token, x-refresh-token'
+    );
+
     next();
 });
 //Routes which should handle requests
