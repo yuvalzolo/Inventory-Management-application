471779f9f6470b8c867f6f2352129e0791f3327b
yuvalzolo <45453175+yuvalzolo@users.noreply.github.com>
2020-09-21 10:57:48 +0300

    Update withdraw-item.component.ts

--- frontend/src/app/pages/withdraw-item/withdraw-item.component.ts
+++ frontend/src/app/pages/withdraw-item/withdraw-item.component.ts
@@ -15,8 +15,25 @@ export class WithdrawItemComponent implements OnInit {
   // tslint:disable-next-line:typedef
   withdrawItem(id: string, count: number){
     this.taskService.withrawItemByid(id, count).subscribe((response: any) => {
-      console.log(response);
+      if (response === null || response === undefined){
+        alert('Item not found');
+      }
+      else{
+        if (response.count === 0){
+          this.taskService.removeItem(id).subscribe((deleteresponse: any) => {
+            console.log(deleteresponse);
+          });
+          alert('The new count is 0 - the item has been removed from stock');
+          window.location.replace('/');
+        }
+        else {
+          if (response.description === 'negative item') {
+            alert('Unable to withdraw more items then you have in stock \n' + `current count: ${response.count}`);
+          } else {
+            window.location.replace('/');
+          }
+        }
+      }
     });
-    window.location.replace('/');
   }
 }
